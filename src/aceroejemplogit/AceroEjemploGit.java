dff541e78878ca44aad512686fa93b5a1362b220
damt101 <damt101@36-22.aglinformatica.es>
2022-04-28 18:52:55 +0200

    versión 2.0

--- src/aceroejemplogit/AceroEjemploGit.java
+++ src/aceroejemplogit/AceroEjemploGit.java
@@ -17,6 +17,7 @@ public class AceroEjemploGit {
     public static void main(String[] args) {
         System.out.println("Pablo Acero");
         System.out.println("Version 1");
+        System.out.println("Version 2");
         // TODO code application logic here
     }
     
